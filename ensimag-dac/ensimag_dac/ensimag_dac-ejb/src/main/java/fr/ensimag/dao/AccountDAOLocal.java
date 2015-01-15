6411b6b3f394bfedd990b71d761b30b922d496d3
Edward <alexrojas235@gmail.com>
2015-01-15 15:01:17 +0100

    DAO for each entity created

--- ensimag-dac/ensimag_dac/ensimag_dac-ejb/src/main/java/fr/ensimag/dao/AccountDAOLocal.java
+++ ensimag-dac/ensimag_dac/ensimag_dac-ejb/src/main/java/fr/ensimag/dao/AccountDAOLocal.java
@@ -1,7 +1,6 @@
 package fr.ensimag.dao;
 
 import fr.ensimag.entity.Account;
-import java.util.List;
 import javax.ejb.Local;
 
 /**
@@ -9,18 +8,6 @@ import javax.ejb.Local;
  * @author Edward
  */
 @Local
-public interface AccountDAOLocal {
-
-    void create(Account account) throws Exception;
-
-    void edit(Account account) throws Exception;
-
-    void remove(Account account) throws Exception;
-
-    Account find(Object id) throws Exception;
-
-    List<Account> findAll() throws Exception;
-
-    int count() throws Exception;
+public interface AccountDAOLocal extends AbstractLocal<Account>{    
 
 }
