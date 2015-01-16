1f1f4f3007289ff165d9dcd64ed73a02e8755943
Antoine Le Caër <antoine.le-caer@ensimag.fr>
2015-01-16 16:38:03 +0100

    commit sql scripts random

---
+++ databd.sql
@@ -0,0 +1,13 @@
+/*=========================================================*/
+/*             Peuplement BDD - Données de test            */
+/*=========================================================*/
+
+/*Rôles des utilisateurs*/
+INSERT INTO ROLE VALUES ('1',"Administrateur","Role principal, possédant tous les droits. Ce compte est principalement présent pour la maintenance du système.");
+
+INSERT INTO ROLE VALUES ('2', "Fournisseur","Role des personnes pouvant ajouter de nouveaux produits sur le site.");
+
+INSERT INTO ROLE VALUES ('3', "Utilisateur","Utilisateur enregistré.");
+
+/*Utilisateurs*/
+
