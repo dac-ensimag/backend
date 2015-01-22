da71c751db367c65a8c6ef798bbbc73b116ed1d2
Salah Chadli <salah.chadli@gmail.com>
2015-01-22 14:30:12 +0100

    Ajout du panier

--- ensimag-dac/ensimag_dac/ensimag_dac-web/src/main/java/fr/ensimag/control/CatalogBean.java
+++ ensimag-dac/ensimag_dac/ensimag_dac-web/src/main/java/fr/ensimag/control/CatalogBean.java
@@ -1,14 +1,15 @@
 package fr.ensimag.control;
 
-import fr.ensimag.logic.CatalogServiceLocal;
-import fr.ensimag.vo.ArticleVO;
+import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import java.io.Serializable;
-import java.util.List;
+
+import fr.ensimag.logic.CatalogServiceLocal;
+import fr.ensimag.vo.ArticleVO;
 
 @ManagedBean
 @RequestScoped
@@ -16,17 +17,54 @@ public class CatalogBean implements Serializable {
 
 	@EJB
 	private CatalogServiceLocal catalog;
-        
 
 	private List<ArticleVO> products;
 
+	/*
+	 * Des articles pour tester
+	 *
+	 * ArticleVO art;
+	 *
+	 * ArticleVO art2;
+	 *
+	 * ArticleVO art3;
+	 */
 	public CatalogBean() {
 	}
 
 	@PostConstruct
 	public void init() {
 		try {
-			this.products = catalog.getAllProducts();
+			products = catalog.getAllProducts();
+			/*
+			 * Ajouter d'articles pour tester
+			 * 
+			 * art = new ArticleVO();
+			 * 
+			 * art2 = new
+			 * 
+			 * ArticleVO(); art3 = new ArticleVO();
+			 * 
+			 * art.setArticleId(1);
+			 * 
+			 * art2.setArticleId(2);
+			 * 
+			 * art3.setArticleId(3);
+			 * 
+			 * art.setArticleLibele("Projet A");
+			 * art2.setArticleLibele("Projet B");
+			 * art3.setArticleLibele("Projet C");
+			 * 
+			 * art.setArticlePrix(100);
+			 * 
+			 * art2.setArticlePrix(200); art3.setArticlePrix(300);
+			 * 
+			 * products.add(art);
+			 * 
+			 * products.add(art2); p
+			 * 
+			 * roducts.add(art3);
+			 */
 		} catch (Exception e) {
 		}
 	}
@@ -43,6 +81,7 @@ public class CatalogBean implements Serializable {
 		return "failure";
 	}
 
+	@Override
 	public String toString() {
 		return getProducts().toString();
 	}
