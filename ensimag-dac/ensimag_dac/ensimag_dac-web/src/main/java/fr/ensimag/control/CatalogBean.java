93def19c14226d258c8ab88a1c461541880f5f84
Salah Chadli <salah.chadli@gmail.com>
2015-01-23 18:31:21 +0100

    Ajout de pagination

--- ensimag-dac/ensimag_dac/ensimag_dac-web/src/main/java/fr/ensimag/control/CatalogBean.java
+++ ensimag-dac/ensimag_dac/ensimag_dac-web/src/main/java/fr/ensimag/control/CatalogBean.java
@@ -4,15 +4,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
+import util.RepeatPaginator;
 import fr.ensimag.logic.CatalogServiceLocal;
 import fr.ensimag.vo.ArticleVO;
 
@@ -25,15 +25,17 @@ public class CatalogBean implements Serializable {
 
 	private List<ArticleVO> products;
 
+	private RepeatPaginator paginator;
+
 	private String error;
 
 	/*
 	 * Des articles pour tester
-	 *
+	 * 
 	 * ArticleVO art;
-	 *
+	 * 
 	 * ArticleVO art2;
-	 *
+	 * 
 	 * ArticleVO art3;
 	 */
 	public CatalogBean() {
@@ -42,49 +44,51 @@ public class CatalogBean implements Serializable {
 	@PostConstruct
 	public void init() {
 		try {
-            products = new ArrayList<ArticleVO>();
+			this.products = new ArrayList<ArticleVO>();
 
 			// Ajouter d'articles pour tester
 
-			ArticleVO art = new ArticleVO();
+			/*
+			 * ArticleVO art = new ArticleVO();
+			 * 
+			 * ArticleVO art2 = new ArticleVO();
+			 * 
+			 * ArticleVO art3 = new ArticleVO();
+			 * 
+			 * art.setArticleId(1);
+			 * 
+			 * art2.setArticleId(2);
+			 * 
+			 * art3.setArticleId(3);
+			 * 
+			 * art.setArticleLibele("Projet A");
+			 * art2.setArticleLibele("Projet B");
+			 * art3.setArticleLibele("Projet C");
+			 * 
+			 * art.setArticlePrix(100);
+			 * 
+			 * art2.setArticlePrix(200); art3.setArticlePrix(300);
+			 * 
+			 * products.add(art); products.add(art2); products.add(art3);
+			 */
 
-			ArticleVO art2 = new ArticleVO();
+			this.products = this.catalog.getAllProducts();
 
-			ArticleVO art3 = new ArticleVO();
-
-			art.setArticleId(1);
-
-			art2.setArticleId(2);
-
-			art3.setArticleId(3);
-
-			art.setArticleLibele("Projet A");
-			art2.setArticleLibele("Projet B");
-			art3.setArticleLibele("Projet C");
-
-			art.setArticlePrix(100);
-
-			art2.setArticlePrix(200);
-			art3.setArticlePrix(300);
-
-			products.add(art);
-			products.add(art2);
-			products.add(art3);
-			products = catalog.getAllProducts();
-		} catch (Exception e) {
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			PrintStream ps = new PrintStream(baos);
+			this.paginator = new RepeatPaginator(this.products);
+		} catch (final Exception e) {
+			final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			final PrintStream ps = new PrintStream(baos);
 			e.printStackTrace(ps);
 			try {
 				this.error = baos.toString("UTF-8");
-			} catch (UnsupportedEncodingException e1) {
+			} catch (final UnsupportedEncodingException e1) {
 				e1.printStackTrace();
 			}
 		}
 	}
 
 	public List<ArticleVO> getProducts() {
-		return products;
+		return this.products;
 	}
 
 	public String view() {
@@ -93,17 +97,19 @@ public class CatalogBean implements Serializable {
 
 	@Override
 	public String toString() {
-		return getProducts().toString();
+		return this.getProducts().toString();
 	}
 
-    public void setError(String error) {
-        this.error = error;
-    }
+	public void setError(final String error) {
+		this.error = error;
+	}
 
-    public String getError() {
-        return this.error;
-    }
+	public String getError() {
+		return this.error;
+	}
 
-
+	public RepeatPaginator getPaginator() {
+		return this.paginator;
+	}
 
 }
