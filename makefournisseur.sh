e271bef17c35fe5aa1b505da2a009f7f912a3dd3
Antoine Le Caër <antoine.le-caer@ensimag.fr>
2015-01-30 13:44:43 +0100

    makefournisseur.sh

---
+++ makefournisseur.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Ce script rend administrateur le login passé en paramètre.
+if [ $# -ne 1 ]
+then
+	echo "Ce script nécessite un (unique) login en paramètre."
+	exit
+fi
+
+echo "UPDATE UTILISATEUR SET ROLE_ID = 2 WHERE UTILISATEUR_LOGIN='"$1"';" > toto.sql
+sudo mysql ENSIMAG_DAC_DB < toto.sql
+rm toto.sql
